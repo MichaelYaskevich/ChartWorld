76c08ca3716fbe5993d345ba96b1a34e37d36d93
MichaelYaskevich <The_person38@mail.ru>
2022-05-15 21:05:46 +0500

    убрал лишние юзинги

--- ChartWorld/Workspace/Workspace.cs
+++ ChartWorld/Workspace/Workspace.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
 using ChartWorld.App;
-using ChartWorld.Chart;
 
 namespace ChartWorld.Workspace
 {
--- ChartWorld/Workspace/WorkspaceEntity.cs
+++ ChartWorld/Workspace/WorkspaceEntity.cs
@@ -1,7 +1,6 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
-using ChartWorld.App;
 
 namespace ChartWorld.Workspace
 {
